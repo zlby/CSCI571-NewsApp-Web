8e59a95b870399032a9b8565bee610b078e403aa
Liangboya Zhou <zlby970129@gmail.com>
2020-04-23 03:18:04 -0700

    modify path

--- client/src/server.js
+++ client/src/server.js
@@ -10,11 +10,11 @@ app.use(compression());
 app.use(morgan("combined"));
 
 // Serve the static files from the build folder
-app.use(express.static( __dirname + "/build"));
+app.use(express.static( __dirname + "/../build"));
 //app.use('/material-dashboard-react', express.static(__dirname + "/build"));
 // Redirect all traffic to the index
 app.get("*", function(req, res){
-    res.sendFile(__dirname + "/build/index.html");
+    res.sendFile(__dirname + "/../build/index.html");
 });
 // Listen to port 3000
 app.listen(process.env.PORT || 3000);
