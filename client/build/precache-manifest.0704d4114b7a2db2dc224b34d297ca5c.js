89f4690b8e4f0ef30105553b3204f92a4c4badda
Liangboya Zhou <zlby970129@gmail.com>
2020-04-23 03:04:36 -0700

    add build

---
+++ client/build/precache-manifest.0704d4114b7a2db2dc224b34d297ca5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da4f86fe034de5321688c4a20b9a31d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abe3445b20b78f4d72e9",
+    "url": "/static/css/2.f39ea103.chunk.css"
+  },
+  {
+    "revision": "368dbd8e497f8582db7c",
+    "url": "/static/css/main.7b8ab67b.chunk.css"
+  },
+  {
+    "revision": "abe3445b20b78f4d72e9",
+    "url": "/static/js/2.4669075a.chunk.js"
+  },
+  {
+    "revision": "aca8f8fc88948883037dd6367532578e",
+    "url": "/static/js/2.4669075a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "368dbd8e497f8582db7c",
+    "url": "/static/js/main.f6a4fb81.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
