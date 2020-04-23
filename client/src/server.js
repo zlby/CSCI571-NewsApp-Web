4d3b3c467d77c018f5931432fd19832e776a2516
Liangboya Zhou <zlby970129@gmail.com>
2020-04-23 03:09:36 -0700

    modify entrypoint

--- client/src/server.js
+++ client/src/server.js
@@ -1,8 +1,20 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+var express = require("express");
+var morgan = require("morgan");
+var compression = require('compression');
+var helmet = require('helmet');
+
+var app = express();
+app.use(helmet());
+app.use(compression());
+
+app.use(morgan("combined"));
+
+// Serve the static files from the build folder
+app.use(express.static( __dirname + "/build"));
+//app.use('/material-dashboard-react', express.static(__dirname + "/build"));
+// Redirect all traffic to the index
+app.get("*", function(req, res){
+    res.sendFile(__dirname + "/build/index.html");
 });
-app.listen(3000);
+// Listen to port 3000
+app.listen(process.env.PORT || 3000);
